24800fec8199da5cf2bb5999d94e22d688699355
OwOY <63274030+OwOY@users.noreply.github.com>
2022-01-21 19:04:07 +0800

    Update UserInfo.py

--- test/APIGUI/UserInfo.py
+++ test/APIGUI/UserInfo.py
@@ -1,4 +1,4 @@
-from flask_restplus import Api, Namespace, fields, Resource
+from flask_restplus import Namespace, Resource
 from flask import request
 from model.UserInfo import md
 
@@ -11,21 +11,16 @@ test1 = api.model('test_model', {
     'test' : fields.Integer(required=True, description="test", help="this is test")
 })
 
+@api.route('/test')
+class Test(Resourse):
+    @api.expect(test)
+    def post(self):
+        data = request.get_json()
+        return srv.test(data)
 
-@api.route('/', methods = ['GET', 'POST'])
-class index(Resource):
-    @api.route('/test')
-    class test1(Resource):
-        @api.expect(test)
-        def post(self):
-            data = request.get_json()
-            output = md().search(data)
-            return output
-
-    @api.route('/test1')
-    class test2(Resource):
-        @api.expect(test1)
-        def post(self):
-            data = request.get_json()
-            md().create(data)
-            return data
+@api.route('/test1')
+class Test1(Resourse):
+    @api.expect(test1)
+    def post(self):
+        data = request.get_json()
+        return srv.test(data)
